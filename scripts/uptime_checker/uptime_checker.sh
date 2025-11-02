88dd3027de12224db462ae533d3c3e24539fead6
Uday-T-Kumar <udaytkumar.consult@gmail.com>
2025-11-02 21:31:28 +0530

    Revert "CSN-1031 Register API : Machine health-check service"
    This reverts commit 9e68be7ea6cb9e6aa95257a424fd44c1f30a8552.

--- scripts/uptime_checker/uptime_checker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Path of Python script
-SCRIPT_PATH="/home/sn27/uptime_checker/uptime_checker.py"
-
-su - sn27 <<'EOF'
-SCRIPT_PATH="/home/sn27/uptime_checker/uptime_checker.py"
-/usr/bin/env python3 "$SCRIPT_PATH"
-EOF
